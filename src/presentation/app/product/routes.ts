ceaa6265c62de4b1b6d59727a3caa9d1b7ee0948
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-03-25 18:49:21 -0300

    Refactor product routes and add product controller and service

--- src/presentation/app/product/routes.ts
+++ src/presentation/app/product/routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import { ProductController } from "./controller";
+import { ProductService } from "./service";
 
 export class ProductRoutes {
     static get routes(): Router {
+                            
         const router = Router()
+        const service = new ProductService()
+        const controller = new ProductController(service)
 
-        router.get("/get-all")
-        router.post("/create")
-        router.put("/update")
-        router.delete("/delete")
+        router.get("/get-all", controller.getAll)
+        router.post("/create", controller.create)
+        router.put("/update", controller.update)
+        router.delete("/delete", controller.delete)
 
         return router
     }
--- src/presentation/server/routes.ts
+++ src/presentation/server/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { ProductRoutes } from "../app/product/routes";
 
 export class AppRouter {
     static get routes(): Router {
         const router = Router();
 
-        //router.use('/api/user', UserRoutes.routes)
+        router.use('/api/products', ProductRoutes.routes)
 
         return router;
     }
