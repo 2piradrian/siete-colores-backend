0dafc1c12cec7c3159ca6d113eae79ac15e5bd59
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-03-25 19:47:32 -0300

    product data source

--- src/infrastructure/data-source/mongo-product.ts
+++ src/infrastructure/data-source/mongo-product.ts
@@ -1,74 +1,102 @@
-import { ProductDataSource, ProductEntity } from "../../domain";
+import { ProductModel } from "../../data";
+import { ErrorType, ProductDataSource, ProductEntity } from "../../domain";
 
 export class MongoProductDataSource implements ProductDataSource {
 
     public async getAll(): Promise<ProductEntity[]> {
         try {
+            const products = await ProductModel.find();
 
+            return products.map(product => ProductEntity.fromObject(product));
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 
     public async getByCode(code: string): Promise<ProductEntity | undefined> {
         try {
+            const product = await ProductModel.findOne({ code });
 
+            if (!product) {
+                return undefined;
+            }
+
+            return ProductEntity.fromObject(product);
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 
     public async getBySeries(series: string): Promise<ProductEntity[] | undefined> {
         try {
+            const products = await ProductModel.find({ 
+                code: { $regex: new RegExp('^' + series, 'i') }
+            });
 
+            if (!products || !products.length) {
+                return undefined;
+            }
+
+            return products.map(product => ProductEntity.fromObject(product));
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 
     public async create(product: ProductEntity): Promise<ProductEntity> {
         try {
+            const productModel = new ProductModel(product);
 
+            await productModel.save();
+
+            return ProductEntity.fromObject(productModel);
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 
     public async update(product: ProductEntity): Promise<ProductEntity> {
         try {
+            const productModel = await ProductModel.findOne({ code: product.code });
 
+            if (!productModel) {
+                throw ErrorType.Unknown;
+            }
+
+            productModel.set(product);
+
+            await productModel.save();
+
+            return ProductEntity.fromObject(productModel);
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 
     public async updateList(productList: ProductEntity[]): Promise<ProductEntity[]> {
         try {
+            for (const product of productList) {
+                await this.update(product);
+            }
 
+            return productList;
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 
     public async delete(code: string): Promise<void> {
         try {
-
+            await ProductModel.deleteOne({ code });
         }
         catch(error){
             throw error
         }
-        throw new Error("Method not implemented.");
     }
 }
