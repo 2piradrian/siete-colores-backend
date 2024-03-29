e69e60966a7c2dba1274f9c10f63b701009a84fa
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-03-29 12:50:20 -0300

    Add budget entity, data source, and repository

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,8 +1,11 @@
 export * from './entity/product';
+export * from './entity/budget';
 
 export * from './data-source/product'
+export * from './data-source/budget'
 
 export * from './repository/product'
+export * from './repository/budget'
 
 export * from './dto/product/create'
 export * from './dto/product/get-by-code'
