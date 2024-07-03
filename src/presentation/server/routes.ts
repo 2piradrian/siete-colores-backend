9d9f38c0740a2c306da0cfa57fd496b409e30444
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-03 10:24:11 -0300

    added category routes

--- src/presentation/server/routes.ts
+++ src/presentation/server/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ProductRoutes } from "../app/product/routes";
 import { BudgetRoutes } from "../app/budget/routes";
 import { StatisticsRoutes } from "../app/statistics/router";
+import { CategoryRoutes } from "../app/category/routes";
 
 export class AppRouter {
     static get routes(): Router {
@@ -10,7 +11,7 @@ export class AppRouter {
         router.use('/api/products', ProductRoutes.routes)
         router.use('/api/budgets', BudgetRoutes.routes)
         router.use('/api/statistics', StatisticsRoutes.routes)
-        router.use('/api/categories')
+        router.use('/api/categories', CategoryRoutes.routes)
 
         return router;
     }
