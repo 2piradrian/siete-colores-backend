2093df3a06638af0a92a74bdb2eabc3b831568d8
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-05 16:20:52 -0300

    feat: Add subtotal and discount properties to BudgetEntity

--- src/domain/entity/budget.ts
+++ src/domain/entity/budget.ts
@@ -5,17 +5,21 @@ export class BudgetEntity {
         public id: string,
         public products: BudgetProductEntity[],
         public client: string,
+        public subtotal: number,
+        public discount: number,
         public total: number,
         public date: Date,
     ) {}
 
     static fromObject(object: {[key: string]: any}): BudgetEntity {
-        const { _id, id, products, client, date, total } = object;
+        const { _id, id, products, client, date, subtotal, discount, total } = object;
 
         return new BudgetEntity(
             _id || id,
             products,
             client,
+            subtotal,
+            discount,
             total,
             date,
         );
